89099ce45e1f1e704974dda0935d41f74a002e00
gustavo-rolim <gustavo.rolim@usp.br>
2025-08-29 17:06:34 -0300

    Added the knapsack solver version 1

--- knapsack_solver.jl
+++ knapsack_solver.jl
@@ -1,14 +1,34 @@
+function solve_knapsack(capacity::Int, l::Vector{Int}, reward::Vector{<:Real})
+    #----- Solve knapsack problem with dynamic programming
 
-function knapsack_solver()
-    println("Hello, Git!")
+    n = length(l)
+    dp = zeros(Float64, n+1, capacity+1)
+
+    #----- Build dynamic programming table
+    for i in 1:n 
+        for w in 1:capacity+1
+            current_capacity = w - 1 
+            if l[i] <= current_capacity
+                dp[i+1, w] = max(dp[i, w], reward[i] + dp[i, w - l[i]])
+            else
+                dp[i+1, w] = dp[i, w]
+            end
+        end
+    end
+
+    #----- Backtracking 
+    w = capacity + 1
+    selected = Int[]
+
+    for i in n:-1:1
+        if dp[i+1, w] != dp[i, w]
+            push!(selected, i)
+            w -= l[i]
+            if w < 1
+                break
+            end
+        end
+    end
+
+    return dp[n+1, capacity+1], reverse(selected)
 end
-
-# Let's add the problem parameters for the knapsack algorithm.
-
-function set_parameters()
-    items = [("item1", 2, 6), ("item2", 3, 4), ("item3", 4, 2)]
-    capacity = 5
-    return items, capacity
-end
-
-knapsack_solver()
