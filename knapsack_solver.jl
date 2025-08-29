f734b0a883b613339bcd10f51dcc3c361edfe8ed
gustavo-rolim <gustavo.rolim@usp.br>
2025-08-28 21:39:44 -0300

    setting parameters

--- knapsack_solver.jl
+++ knapsack_solver.jl
@@ -3,4 +3,12 @@ function knapsack_solver()
     println("Hello, Git!")
 end
 
+# Let's add the problem parameters for the knapsack algorithm.
+
+function set_parameters()
+    items = [("item1", 2, 6), ("item2", 3, 4), ("item3", 4, 2)]
+    capacity = 5
+    return items, capacity
+end
+
 knapsack_solver()
